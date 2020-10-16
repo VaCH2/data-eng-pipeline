b77ec48221f55306d68f5452ef0c8162936836d4
vach <chiel.van.asseldonk@hotmail.com>
2020-10-16 19:08:02 +0200

    switched to bash

--- setup_GCP_client.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-kubectl create serviceaccount servicedataeng -n kube-system
-kubectl create clusterrolebinding servicedataeng --clusterrole=cluster-admin --serviceaccount kube-system:servicedataeng
-
-pause
-cmd /k
